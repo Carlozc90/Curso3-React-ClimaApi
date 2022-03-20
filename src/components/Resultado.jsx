065034502cfc454c366325d8d379edbb02d2d81c
Carlos Cancines <Carlozc90@gmail.com>
2022-03-19 22:32:22 -0500

    finalizado

---
+++ src/components/Resultado.jsx
@@ -0,0 +1,33 @@
+import useClima from "../hooks/useClima";
+
+const Resultado = () => {
+  const { resultado, loading } = useClima();
+  const { name, main } = resultado;
+  const kelvin = 273.15;
+  return (
+    <div className="contenedor">
+      {!loading ? (
+        <>
+          {" "}
+          <h2>El Clima de {name} es: </h2>
+          <p>
+            Temperatura Actual: {parseInt(main.temp - kelvin)}{" "}
+            <span>&#x2103;</span>
+          </p>
+          <p>
+            Temperatura Minima: {parseInt(main.temp_min - kelvin)}{" "}
+            <span>&#x2103;</span>
+          </p>
+          <p>
+            Temperatura Maxima: {parseInt(main.temp_max - kelvin)}{" "}
+            <span>&#x2103;</span>
+          </p>
+        </>
+      ) : (
+        <p>Cargando....</p>
+      )}
+    </div>
+  );
+};
+
+export default Resultado;
